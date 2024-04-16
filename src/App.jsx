9db79a5142e5c905fc97a47af5e23ec829890a8d
marwan-pixel <marwanhadid6@gmail.com>
2024-04-16 22:39:07 +0700

    Update Progress Project GearOne (UI Design)

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import Home from "./components/Home.jsx";
-import "./App.css";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("Hello");
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
-
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <input type="text" value={text} onChange={handleChange} />
-        <p>{text}</p>
-      </div>
-
       <Home></Home>
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,79 @@
 import * as React from "react";
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
 
-function Home() {
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function ButtonAppBar() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClickClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <>
-      <Button variant="contained">Hello World</Button>
-    </>
+    <React.Fragment>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ backgroundColor: "#E72929" }}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            ></IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, fontFamily: "roboto" }}
+            >
+              GearOne
+            </Typography>
+            <Button
+              variant="outlined"
+              color="inherit"
+              sx={{ mr: 2, fontFamily: "roboto", textTransform: "none" }}
+            >
+              <Typography>Sign Up</Typography>
+            </Button>
+            <Button
+              onClick={handleClickOpen}
+              variant="text"
+              color="inherit"
+              sx={{ textTransform: "none" }}
+            >
+              <Typography>Login</Typography>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <BootstrapDialog></BootstrapDialog>
+    </React.Fragment>
   );
 }
-
-export default Home;

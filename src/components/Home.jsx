ffef1682ee8558050d3363d098050efe5712200d
marwan-pixel <marwanhadid6@gmail.com>
2024-04-17 15:51:24 +0700

    Update Progress Project GearOne (Desain Form Login)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,25 @@
 import * as React from "react";
 import { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import { styled } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1),
-  },
-}));
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  AppBar,
+  Box,
+  Typography,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Toolbar,
+  Stack,
+  Checkbox,
+  Avatar,
+  FormControlLabel,
+  Container,
+} from "@mui/material";
 
 export default function ButtonAppBar() {
   const [open, setOpen] = useState(false);
@@ -35,20 +32,19 @@ export default function ButtonAppBar() {
     setOpen(false);
   };
 
+  const defaultTheme = createTheme();
   return (
-    <React.Fragment>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{ backgroundColor: "#E72929" }}>
+    <ThemeProvider theme={defaultTheme}>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <AppBar
+          position="static"
+          sx={{
+            backgroundColor: "#E72929",
+          }}
+        >
           <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            ></IconButton>
             <Typography
-              variant="h6"
+              variant="h5"
               component="div"
               sx={{ flexGrow: 1, fontFamily: "roboto" }}
             >
@@ -57,7 +53,11 @@ export default function ButtonAppBar() {
             <Button
               variant="outlined"
               color="inherit"
-              sx={{ mr: 2, fontFamily: "roboto", textTransform: "none" }}
+              sx={{
+                mr: 2,
+                fontFamily: "roboto",
+                textTransform: "none",
+              }}
             >
               <Typography>Sign Up</Typography>
             </Button>
@@ -65,15 +65,78 @@ export default function ButtonAppBar() {
               onClick={handleClickOpen}
               variant="text"
               color="inherit"
-              sx={{ textTransform: "none" }}
+              sx={{ textTransform: "none", "&:hover": "#0066CC" }}
             >
-              <Typography>Login</Typography>
+              <Typography>Sign In</Typography>
             </Button>
           </Toolbar>
         </AppBar>
       </Box>
 
-      <BootstrapDialog></BootstrapDialog>
-    </React.Fragment>
+      <Dialog
+        fullWidth
+        maxWidth="sm"
+        onClose={handleClickClose}
+        aria-labelledby="dialog-title"
+        open={open}
+      >
+        <Container component="main" maxWidth="sm">
+          <CssBaseline />
+          <Typography mt={4} mb={1} ml={3} component="h1" variant="h4">
+            GearOne
+          </Typography>
+          <div className="border mx-5"></div>
+          <Typography
+            mt={1}
+            ml={3}
+            textTransform={"uppercase"}
+            component="h1"
+            variant="h5"
+          >
+            Sign In
+          </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Box component="form" noValidate sx={{ mt: 2, width: 500 }}>
+              <TextField
+                sx={{ marginBottom: 3 }}
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                type="text"
+              ></TextField>
+              <TextField
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+              ></TextField>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember Me"
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                autoFocus
+                onClick={handleClickClose}
+                sx={{ mt: 5, mb: 4, backgroundColor: "#E72929" }}
+                size="large"
+              >
+                Login
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </Dialog>
+    </ThemeProvider>
   );
 }
